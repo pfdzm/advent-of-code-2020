d40be2eaa67e7e3171070491ee7eece49a098e2b
pfdzm <pfdzm@users.noreply.github.com>
2020-12-12 22:29:24 +0100

    Day 4, Part 2 solved

--- d04-passport-processing/src/index.ts
+++ d04-passport-processing/src/index.ts
@@ -15,8 +15,18 @@ const readFile = (filename: string): string => {
     pid (Passport ID)
     cid (Country ID)
  */
+type passportKeys =
+  | "byr"
+  | "iyr"
+  | "eyr"
+  | "hgt"
+  | "hcl"
+  | "ecl"
+  | "pid"
+  | "cid";
+
 type passport = {
-  [index: string]: string;
+  [index in passportKeys]?: string;
 };
 
 const parsePassport: (
@@ -41,7 +51,7 @@ const parsePassport: (
   }
 
   const keyValue = line.slice(prevPos, pos).split(":");
-  const key = keyValue[0];
+  const key = keyValue[0] as passportKeys;
   const value = keyValue[1];
   parsedInput[key] = value;
 
@@ -64,7 +74,7 @@ const validate = (passport: passport): boolean => {
   let validKeys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
   for (const prop in passport) {
     const index = validKeys.indexOf(prop);
-    if (index !== -1) {
+    if (index !== -1 && validateValue(passport, prop as passportKeys)) {
       D++;
       validKeys = validKeys
         .slice(0, index)
@@ -72,7 +82,62 @@ const validate = (passport: passport): boolean => {
     }
   }
   D++;
-  return 8 === D;
+  return 8===D;
+};
+
+/**
+byr (Birth Year) - four digits; at least 1920 and at most 2002.
+iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+hgt (Height) - a number followed by either cm or in:
+If cm, the number must be at least 150 and at most 193.
+If in, the number must be at least 59 and at most 76.
+hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+pid (Passport ID) - a nine-digit number, including leading zeroes.
+cid (Country ID) - ignored, missing or not.
+*/
+
+const validateValue = (passport: passport, prop: passportKeys) => {
+  const value = passport[prop] as string;
+  switch (prop) {
+    case "byr":
+      const byr = parseInt(value);
+      return byr >= 1920 && byr <= 2002;
+    case "iyr":
+      const iyr = parseInt(value);
+      return iyr >= 2010 && iyr <= 2020;
+    case "eyr":
+      const eyr = parseInt(value);
+      return eyr >= 2020 && eyr <= 2030;
+    case "hgt":
+      const unit = value.slice(value.length - 2);
+      const number = parseInt(value.slice(0, value.length - 2));
+      const validNumber =
+        unit === "cm"
+          ? number >= 150 && number <= 193
+          : number >= 59 && number <= 76;
+      return (
+        value.length > 2 && validNumber && (unit === "cm" || unit === "in")
+      );
+    case "hcl":
+      const regex = /^#[a-f0-9]{6}$/;
+      return regex.test(value);
+    case "ecl":
+      return (
+        value === "amb" ||
+        value === "blu" ||
+        value === "brn" ||
+        value === "gry" ||
+        value === "grn" ||
+        value === "hzl" ||
+        value === "oth"
+      );
+    case "pid":
+      return /^[0-9]{9}$/.test(value);
+    default:
+      throw new Error("Validation Error! Unknown passport key!");
+  }
 };
 
 const exampleInput = readFile("example.txt").split("\n");
