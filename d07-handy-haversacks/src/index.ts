c6c1f2a9914251faa532d80d6936aa70b56d810c
pfdzm <pfdzm@users.noreply.github.com>
2020-12-13 20:07:50 +0100

    Day 7, Part 1 solved

--- d07-handy-haversacks/src/index.ts
+++ d07-handy-haversacks/src/index.ts
@@ -5,10 +5,8 @@ const readFile = (filename: string): string => {
   return fs.readFileSync(path.join(process.cwd(), 'input', filename)).toString()
 }
 
-const writeOutput = (data: Record<string, string | number>[]) => {
-  const outputStr = data.reduce((prev, curr) => {
-    return prev + JSON.stringify(curr) + '\n'
-  }, '')
+const writeOutput = (data: Record<string, string[]>) => {
+  const outputStr = JSON.stringify(data, null, 2)
   fs.writeFileSync(path.join(process.cwd(), 'output.txt'), outputStr, 'utf-8')
 }
 
@@ -21,23 +19,51 @@ function parseBagRule(line: string): any {
   const rules = rule[1]
     .split(', ')
     .map((rule) => rule.replace(/\d\s(\w+\s\w+)\sbags?\.?/gi, '$1'))
-
   ruleObj[rule[0]] = [...rules]
   return ruleObj
 }
 
-function parseInput(input: string[]): {} {
+function parseInput(input: string[]): Record<string, string[]> {
   return input.reduce((prev, curr) => {
     return { ...prev, ...parseBagRule(curr) }
-  }, [])
+  }, {})
+}
+
+function findShinyGold(rules: Record<string, string[]>): string[] {
+  let shinyGolds: string[] = []
+
+  // first find all rules that contain shiny golds directly
+  for (const key in rules) {
+    if (Object.prototype.hasOwnProperty.call(rules, key)) {
+      const rule = rules[key]
+      if (rule.includes('shiny gold')) {
+        shinyGolds.push(key)
+      }
+    }
+  }
+
+  // loop through again to find all indirect shiny golds
+  for (let i = 0; i < shinyGolds.length; i++) {
+    const indirects = shinyGolds[i]
+    for (const key in rules) {
+      if (Object.prototype.hasOwnProperty.call(rules, key)) {
+        const rule = rules[key]
+        if (rule.includes(indirects) && !shinyGolds.includes(key)) {
+          shinyGolds.push(key)
+        }
+      }
+    }
+  }
+  return shinyGolds
 }
 
 const example1 = readFile('example.txt').split('\n')
 example1.pop()
 
-parseInput(example1)
-
 const part1 = readFile('input.txt').split('\n')
 part1.pop()
 
-console.log(parseInput(part1))
+// console.log(parseInput(part1))
+
+console.log('example', findShinyGold(parseInput(example1)).length)
+console.log('part 1', findShinyGold(parseInput(part1)).length)
