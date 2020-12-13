26d7ae1309566564b3c71264016134217fe6f256
pfdzm <pfdzm@users.noreply.github.com>
2020-12-13 01:42:41 +0100

    Day 5, part 2 solved

--- d05-binary-boarding/src/index.ts
+++ d05-binary-boarding/src/index.ts
@@ -1,3 +1,5 @@
+import { write } from "fs";
+
 const fs = require("fs");
 const path = require("path");
 
@@ -22,10 +24,15 @@ const binarySearch = (seq: string, size: number) => {
   return end;
 };
 
-const binaryBoard = (sequence: string) => {
+interface SeatInfo extends Record<string, number> {
+  row: number;
+  col: number;
+  seatId: number;
+}
+const binaryBoard: (seq: string) => SeatInfo = (sequence) => {
   const row = binarySearch(sequence.slice(0, 7), 128);
-  const col = binarySearch(sequence.slice(7, 9), 8);
-  return row * 8 + col;
+  const col = binarySearch(sequence.slice(7), 8);
+  return { row, col, seatId: row * 8 + col } as SeatInfo;
 };
 
 const binaryBoardInput = (filename: string) => {
@@ -35,8 +42,13 @@ const binaryBoardInput = (filename: string) => {
   return input.map((seq) => binaryBoard(seq));
 };
 
-const findHighestSeatID = (seats: number[]) => {
-  function compareFn(a: number, b: number) {
+const sortSeatsByRow = (seats: SeatInfo[]) => {
+  function compareFn(
+    aObj: Record<string, number>,
+    bObj: Record<string, number>
+  ) {
+    let a = aObj["row"];
+    let b = bObj["row"];
     if (a > b) {
       return 1;
     } else if (a < b) {
@@ -45,8 +57,56 @@ const findHighestSeatID = (seats: number[]) => {
       return 0;
     }
   }
-  return seats.sort(compareFn).pop();
+  return seats.sort(compareFn);
+};
+const sortSeatsById = (seats: SeatInfo[]) => {
+  function compareFn(
+    aObj: Record<string, number>,
+    bObj: Record<string, number>
+  ) {
+    let a = aObj["seatId"];
+    let b = bObj["seatId"];
+    if (a > b) {
+      return 1;
+    } else if (a < b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+  return seats.sort(compareFn);
 };
 
-console.log(findHighestSeatID(binaryBoardInput("example.txt")));
-console.log(findHighestSeatID(binaryBoardInput("input.txt")));
+const writeOutput = (seats: SeatInfo[]) => {
+  const outputStr = seats.reduce((prev, curr) => {
+    return prev + JSON.stringify(curr) + "\n";
+  }, "");
+  fs.writeFileSync(path.join(process.cwd(), "output.txt"), outputStr, "utf-8");
+};
+
+const findSeat = (seats: SeatInfo[]) => {
+  let seatId = seats[0]["seatId"];
+  for (let i = 0; i < seats.length; i++) {
+    if (seatId++ !== seats[i]["seatId"]) {
+      // we found a match, step back once to undo post-increment
+      seatId--;
+      break;
+    }
+  }
+  return seatId;
+};
+
+// example / sanity check
+console.log('Example')
+console.log(sortSeatsById(binaryBoardInput("example.txt")).pop());
+
+// part 1
+console.log('Part 1')
+console.log(sortSeatsById(binaryBoardInput("input.txt")).pop());
+
+// part 2
+console.log('Part 2')
+console.log(findSeat(sortSeatsById(binaryBoardInput("input.txt"))));
+
+// write out all seat info to output.txt
+writeOutput(sortSeatsById(binaryBoardInput("input.txt")));
